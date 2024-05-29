d0b049dfcf372b97d106dc0491e1c2a23d97ade4
Antony Gradillas <50510640+avgra3@users.noreply.github.com>
2024-05-29 11:01:19 -0700

    Formatting and adding execute method to class

--- src/contextManager/contextManager.py
+++ src/contextManager/contextManager.py
@@ -1,32 +1,61 @@
 import mariadb
 
+
 # This will implement a context manager to work with MariaDB
 class MariaDBCM:
-    def __init__(self, host: str, user: str, password: str, database: str, port: int):
-        print('Initializing connection...\n')
+    def __init__(
+        self,
+        host: str,
+        user: str,
+        password: str,
+        database: str,
+        port: int,
+        return_dict: bool = False,
+        prepared: bool = False,
+    ):
+        print("Initializing connection...\n")
         self.user = user
         self.password = password
         self.host = host
         self.port = port
         self.database = database
+        self.return_dict = return_dict
+        self.prepared = prepared
         # Makes our connection to mariadb
-        self.conn = mariadb.connect(user=self.user,
-                               password=self.password,
-                               host=self.host,
-                               port=self.port,
-                               database=self.database)
+        self.conn = mariadb.connect(
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port,
+            database=self.database,
+            dictionary=self.return_dict,
+            prepared=self.prepared,
+        )
         self.cur = self.conn.cursor()
 
     def __enter__(self):
-        # If you want to verify your connection entries were correct uncomment the below, otherwise it is not needed
-        #print(f'You entered: {self.host}, {self.user}, {self.password}, {self.database}, {self.port}\n')
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.conn.close()
-        print(f'\nConnection has been closed...\n')
+        print(f"\nConnection has been closed...\n")
         if exc_type:
-            print(f'exc_type: {exc_type}')
-            print(f'exc_value: {exc_value}')
-            print(f'traceback: {traceback}')
+            print(f"exc_type: {exc_type}")
+            print(f"exc_value: {exc_value}")
+            print(f"traceback: {traceback}")
         return True
+
+    def execute(self, query: str) -> dict:
+        result = {}
+        cols = []
+        with self.conn as conn:
+            self.cur.execute(query)
+            for item in list(self.cur.description):
+                cols.append(item[0])
+            result["columns"] = cols
+            result["statement_ran"] = self.cur.statement
+            result["warnings"] = self.cur.warnings
+            if self.cur.rowcount > 0:
+                result["data"] = self.cur.fetchall()
+            self.cur.close()
+        return result
