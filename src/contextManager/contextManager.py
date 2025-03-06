9d17886cba454eccbe22c0e62689a04a27bbb16f
avgra3 <antonygradillas@gmail.com>
2025-03-06 13:14:11 -0700

    Updated for logging instead of printing. Modifying conversions, addition of __slots__ to reduce memory overhead and prevent unncessary attributes

--- src/contextManager/contextManager.py
+++ src/contextManager/contextManager.py
@@ -1,10 +1,14 @@
 import mariadb
 from .conversions import conversions
 from .combined_types import make_type_dictionary
+import logging
 
 
-# This will implement a context manager to work with MariaDB
 class MariaDBCM:
+    __slots__ = ("host", "user", "password", "database", "port", "buffered",
+                 "converter", "return_dict", "prepared", "allow_local_infile",
+                 "conn", "cur")
+
     def __init__(
         self,
         host: str,
@@ -35,34 +39,46 @@ class MariaDBCM:
             port=self.port,
             database=self.database,
             local_infile=self.allow_load_infile,
+            converter=conversions,
         )
-        self.cur = self.conn.cursor()
 
     def __enter__(self):
-        print(f"Connection to {self.database} was made")
+        '''Information that there was a successful connection to the database.'''
+        logging.info(f"Connection to {self.database} was made")
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
+        '''Upon exit, the connection to the database is closed.'''
         self.conn.close()
-        print("\nConnection has been closed...\n")
+        logging.info("\nConnection has been closed...\n")
         if exc_type:
-            print(f"exc_type: {exc_type}")
-            print(f"exc_value: {exc_value}")
-            print(f"traceback: {traceback}")
+            logging.error(f"exc_type: {exc_type}")
+            logging.error(f"exc_value: {exc_value}")
+            logging.error(f"traceback: {traceback}")
         return self
 
     def __check_connection_open(self) -> bool:
+        """Checks that the connection to the database is open.
+        Returns False if the connection is closed, otherwise open."""
         if self.conn.cursor().closed:
             return False
         return True
 
     def __remove_comments(self, query: str) -> str:
+        """Removes comments from a given query
+        query: str, the SQL statement that is used."""
         updated_query = ""
         for line in query.splitlines():
             if not (line.strip()).startswith("--"):
                 updated_query += line.strip()
 
-    def execute_change(self, statement: str, parameters: tuple) -> dict:
+    def execute_change(
+        self, statement: str, parameters: tuple
+    ) -> dict[str, any]:
+        '''statement: The SQL update script
+        parameters: that are used in the update.
+        Returns a dictionary of information from results of changes.'''
+        self.make_cursor()
         if statement.strip() != "" and parameters is not None:
             ran_statement = self.cur.execute_many(statement, parameters)
             statement_results = {
@@ -71,8 +87,12 @@ class MariaDBCM:
                 "number_of_warnings": ran_statement.warnings,
             }
             return statement_results
+        return {}
 
-    def execute(self, query: str) -> dict:
+    def execute(self, query: str) -> dict[dict, any]:
+        '''Execute a SQL query. This can be used for
+        updates, deletes, inserts which do not need parameters.
+        query: str which contains the SQL query ran.'''
         result = {}
         if query.strip() != "":
             with self.conn as conn:
@@ -95,16 +115,33 @@ class MariaDBCM:
 
         return result
 
-    def execute_many(self, queries: str) -> list:
+    def execute_many(self, queries: str) -> list[dict[str, any]]:
+        '''Similar to execute but allows for many queries to be ran
+        sequentially.
+        Note: This is not a sophisticated query execution and expects that
+        all queries are delimited by a ";", and there are no semicolons
+        used within queries.
+        queries: str, run many queries.
+        Returns a list of dictionaries from the execute method.'''
         results = []
         for query in queries.strip().split(";"):
             result = self.execute(query)
             results.append(result)
         return results
 
-    def execute_stored_procedure(self, stored_procedure_name: str, inputs: tuple = ()):
+    def execute_stored_procedure(
+        self, stored_procedure_name: str, inputs: tuple = (),
+    ) -> dict[str, any]:
+        '''Execution of stored procedures.
+        stored_procedure_name: str, the name of the stored procedure.
+        inputs: tuple, all inputs that would be needed for the given
+            stored procedure.
+        Note: this assumes the stored procedure exists and the user
+            knows the needed parameters.
+        Returns a dictionary similar to execute.'''
         with self.conn as conn:
-            cursor = conn.cursor(dictionary=self.return_dict, prepared=self.prepared)
+            cursor = conn.cursor(
+                dictionary=self.return_dict, prepared=self.prepared)
             cursor.callproc(stored_procedure_name, inputs)
             result = {}
             metadata = cursor.metadata
