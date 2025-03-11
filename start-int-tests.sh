ac8cc201d3735d4177d4dbef693fb1fca1e5c418
avgra3 <antonygradillas@gmail.com>
2025-03-11 16:54:09 -0700

    Remove unnecessary commands

--- start-int-tests.sh
+++ start-int-tests.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Create docker network
-# docker network create --subnet=172.20.0.0/16 testing_MCM
-
-# Prune networks
-docker network prune --force
-docker system prune --all --force
-
 # Set env variable
 # export COMPOSE_BAKE=true
 
