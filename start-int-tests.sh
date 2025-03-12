339bd054f74f30d03465b8dabe8945a2e4f8d4ea
avgra3 <antonygradillas@gmail.com>
2025-03-11 18:21:08 -0700

    Added wait for db to be online before running python tests

--- start-int-tests.sh
+++ start-int-tests.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Set env variable
-# export COMPOSE_BAKE=true
+export COMPOSE_BAKE=true
 
 # Main script to build and test our database
 docker compose up --force-recreate \
+	--abort-on-container-exit \
 	-y
 
